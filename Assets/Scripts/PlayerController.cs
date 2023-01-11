b147f33d838f5388e9d1bc1017dfda9692a009c4
RemiRaff <remi_raffaghello@hotmail.com>
2023-01-11 11:28:29 +0100

    RigidBody setup and AddForce 1rst implementation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRB;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRB = GetComponent<Rigidbody>();
+        playerRB.AddForce(Vector3.up * 500); // transform.translate
     }
 
     // Update is called once per frame
