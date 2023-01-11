05f20be07e9037519c3cd41eb118e98b2afe0714
RemiRaff <remi_raffaghello@hotmail.com>
2023-01-11 12:15:55 +0100

    gravityModifier and jumpForce variables added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,12 +5,16 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float jumpForce = 10;
+    public float gravityModifier = 3;
+
     private Rigidbody playerRB;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
@@ -22,6 +26,6 @@ public class PlayerController : MonoBehaviour
     public void Jump(InputAction.CallbackContext ctx)
     {
         if (ctx.started)
-            playerRB.AddForce(Vector3.up * 500); // transform.translate
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); // transform.translate
     }
 }
